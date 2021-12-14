06f0b12b676b4398e9f14594ebbd5bd0a7f90520
amaury-LC <amaury.le.chaffotec@laposte.net>
2021-12-14 11:34:35 +0100

    premier test talon passer

--- domino2.py
+++ domino2.py
@@ -1,4 +1,4 @@
-
+import random
 class Domino:
 
    valeur_a_gauche = 0
@@ -81,33 +81,124 @@ assert mon_autre_domino.est_compatible(4) == True
 assert mon_autre_domino.est_compatible(5) == False
 assert mon_autre_domino.est_compatible(0) == False
 
-class Chaine:
+class Talon:
     """
-    Représentation de la chaîne de jeu
-    exemple: [1: ][3:5][5:2]
-    En plus de la liste ordonnée des dominos selon le placement,
-    Deux valeurs seront stockées:
-    valeur_a_gauche dans notre exemple 1
-    valeur_a_droite dans notre exemple 2
+    Talon (pioche) du jeu de domino
     """
 
-    def __init__(self):
+    def __init__(self, t=None):
+
+        if t == None:
+
+            dominos = [0,1,2,3,4,5,6]
+            dominos2 = [0,1,2,3,4,5,6]
+            liste_domino = []
+            liste_domino_talons = []
+
+            for x in dominos:
+
+                for y in dominos2:
+
+                    d1 = Domino(x,y)
+                    # self.liste_domino.append(d1)
+                    liste_domino.append(Domino(x,y))
+                dominos2.pop(0)
+
+            #choisir 10
+
+            for x in range(10):
+
+                domino_talon = random.choice(liste_domino)
+
+                liste_domino_talons.append(domino_talon)
+                liste_domino.remove(domino_talon)
+            
+            self.t  = liste_domino_talons
+
+            
+        else:
+
+            self.t = t
+            
+
+
+
+
+
+        
+        
+
+    def pioche(self):
         pass
-
-
-    def extremites(self):
-        pass
-
-    def pose_premier(self, domino):
-        pass
-
-    def ajoute_au_cote_gauche(self, domino):
-        pass
-
-
-    def ajoute_au_cote_droit(self, domino):
-        pass
-
 
     def __repr__(self):
         pass
+
+# Test constructeur
+mon_domino = Domino(2, 5)
+mon_autre_domino = Domino(1, 5)
+talon = Talon([mon_domino, mon_autre_domino])
+assert len(talon.t) == 2
+talon = Talon()
+assert len(talon.t) == 10
+
+
+
+# class Chaine:
+#     """
+#     Représentation de la chaîne de jeu
+#     exemple: [1: ][3:5][5:2]
+#     En plus de la liste ordonnée des dominos selon le placement,
+#     Deux valeurs seront stockées:
+#     valeur_a_gauche dans notre exemple 1
+#     valeur_a_droite dans notre exemple 2
+#     """
+
+#     def __init__(self):
+#         pass
+
+
+#     def extremites(self):
+#         pass
+
+#     def pose_premier(self, domino):
+#         self.valeur_a_gauche = domino.valeur_a_gauche
+#         self.valeur_a_droite = domino.valeur_a_droite
+        
+
+#     def ajoute_au_cote_gauche(self, domino):
+#         pass
+
+
+#     def ajoute_au_cote_droit(self, domino):
+#         pass
+
+
+#     def __repr__(self):
+#         pass
+
+
+# chaine = Chaine()
+# mon_domino = Domino(0, 5)
+# chaine.pose_premier(mon_domino)
+# assert chaine.valeur_a_gauche == 0
+# assert chaine.valeur_a_droite == 5
+
+# # Les dominos:
+# # [3:5], [5:2], [1: ]
+
+# premier_domino = Domino(3, 5)
+# deuxieme_domino = Domino(2, 5)
+# troisieme_domino = Domino(1, 0)
+
+# # Placement
+# #      [3:5]
+# #      [3:5][5:2]
+# # [1: ][3:5][5:2]
+
+# chaine = Chaine()
+# chaine.pose_premier(premier_domino)
+# chaine.ajoute_au_cote_droit(deuxieme_domino.inverse())
+# chaine.ajoute_au_cote_gauche(troisieme_domino)
+# assert chaine.valeur_a_gauche == 1
+# assert chaine.valeur_a_droite == 2
